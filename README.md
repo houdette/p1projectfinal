7016378510b4543034eba8a67608f680a321da47
NewUser <houdette.ben@gmail.com>
2014-09-20 11:02:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+
+
+
+
+##P1-project Created by: Houda Benqassem
+
+
+## Live URL
+http://p1.doudette.practice-dwa15.me/html/
+
+## Description
+
+BODY tag:
+- is using the google font Open Sans
+
+DIV container:
+- has a limit width of 50%
+- is centered with margin:0 auto;
+
+Titles:
+- Titles use H1, H2 html tags
+
+Image:
+- the border of the image is acomplished with: background: #eee; padding: 20px;
+
+## Demo
+
+## Details for teaching team
+* No login required
+* Open the file index.html in any browser to view the Web page
+
+## Outside code
+* Google font used from https://www.google.com/fonts#UsePlace:use/Collection:Open+Sans
+
+
+
---
+++ index.php
@@ -0,0 +1,94 @@
+
+
+
+   
+      <!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>My-P1-Houda </title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+		body{
+		font-family: 'Open Sans', sans-serif;
+		}
+
+		h1 {
+			text-align:center;
+		}
+
+		.container {
+			text-align: justify;
+			width:50%;
+			margin: 0 auto;
+		}	
+
+		img {
+			margin: 0 auto;
+			display: block;
+			width: 250px;
+			background: #eee;
+			padding: 20px;
+		}
+	</style>
+	
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Houda Benqassem <br> Dynamic Web Apps Course</h1>
+		
+		<img src="images/me.jpeg"> 
+		
+	
+	
+		
+		<h3>Second Project</h3>
+		View: Link: <a href="http://p2.doudette.practice-dwa15.me/html">[link]</a> Github: <a href="https://github.com/houdette/p2-project">[link]</a>
+		
+		<h3>Third Project</h3>
+		View: Link: <a href="http://p3.doudette.practice-dwa15.me/html">[link]</a> Github: <a href="https://github.com/houdette/p3-project">[link]</a>
+		
+		<h3>Fourth Project</h3>
+		View: Link: <a href="http://p4.doudette.practice-dwa15.me/html">[link]</a> Github: <a href="https://github.com/houdette/p4-project">[link]</a>
+		
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>I am originally from Morocco. I came to the U.S. seven years ago to pursue new career paths. I currently work for an international organization of Finance.  </p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>I am taking this course, because I would like to acquire a certificate in Web Development from Harvard. This class is part of this program. I believe this class will tremendously help me adding more emphasis to my Master's in Information Systems.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have a Master's degree in information systems. I have experience IN HTML and CSS and I took a C course a few months back.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working on MAC.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p> English is my third language. Apologies in advance for any mistakes. </p>
+	
+	</div>
+	
+	</body>
+</html>
+
+
+
+ 
+      
+      
+      
+      
+    
+    
+    
+   
+
+
+
+
+
+
